212be477d2325e2378dde323c5a4c6c9bd6f89b0
rabbanidev <golamrabbani227417@gmail.com>
2023-07-15 23:33:07 +0600

    redux react-redux setup

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+const config = {
+  env: import.meta.env.VITE_ENV as string,
+  base_url: import.meta.env.VITE_BASE_URL as string,
+};
+
+export default config;
---
+++ src/redux/features/api/apiSlice.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import config from "../../../config";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: config.base_url,
+    // prepareHeaders: (headers, { getState }) => {
+    //   const token = (getState() as RootState).auth.token
+    //   if (token) {
+    //     headers.set('authorization', token)
+    //   }
+    //   return headers
+    // }
+  }),
+  endpoints: () => ({}),
+});
+
+export default api;
